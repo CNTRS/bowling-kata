cfcf0c6c469a411faa365fe8c3c2da75891a89ae
CNTRS <m.contreras.calzon@gmail.com>
2025-08-05 01:19:56 +0200

    Add logic for calculate frame score and enable all tests

--- src/__tests__/bowlingGame.test.ts
+++ src/__tests__/bowlingGame.test.ts
@@ -20,7 +20,7 @@ describe("The Bowling Game", () => {
         expect(game.calculateTotalScore()).toBe(20)
     })
 
-    it.skip("calculates the score for a given one spare and extra roll", () => {
+    it("calculates the score for a given one spare and extra roll", () => {
         rollSpare()
         game.roll(5)
         rollMany(17, 0)
@@ -28,7 +28,7 @@ describe("The Bowling Game", () => {
         expect(game.calculateTotalScore()).toBe(20)
     })
 
-    it.skip("calculates the score for a given one strike and some extra rolls", () => {
+    it("calculates the score for a given one strike and some extra rolls", () => {
         rollStrike()
         game.roll(2)
         game.roll(3)
@@ -37,13 +37,13 @@ describe("The Bowling Game", () => {
         expect(game.calculateTotalScore()).toBe(20)
     })
 
-    it.skip("calculates the score for a given perfect game", () => {
+    it("calculates the score for a given perfect game", () => {
         rollMany(12, 10)
 
         expect(game.calculateTotalScore()).toBe(300)
     })
 
-    it.skip("calculates the score for a given all spares game", () => {
+    it("calculates the score for a given all spares game", () => {
         Array.from({ length: 10 }).forEach(rollSpare)
         game.roll(10)
 
@@ -51,7 +51,7 @@ describe("The Bowling Game", () => {
     })
 
     describe("Comparing scores of two all spares games with different spare patterns", () => {
-        it.skip("calculates the score for a game with all 5-5 spares", () => {
+        it("calculates the score for a game with all 5-5 spares", () => {
             const gameA = new BowlingGame()
             Array.from({ length: 10 }).forEach(() => {
                 gameA.roll(5)
@@ -62,7 +62,7 @@ describe("The Bowling Game", () => {
             expect(gameA.calculateTotalScore()).toBe(150)
         })
 
-        it.skip("calculates the score for a game with all 8-2 spares", () => {
+        it("calculates the score for a game with all 8-2 spares", () => {
             const gameB = new BowlingGame()
             Array.from({ length: 10 }).forEach(() => {
                 gameB.roll(8)
--- src/bowlingGame.ts
+++ src/bowlingGame.ts
@@ -1,4 +1,6 @@
 export class BowlingGame {
+    private readonly maxScorePerFrame = 10;
+    private readonly maxFrames = 10;
     private rolls: number[] = [];
 
     roll(pins: number): void {
@@ -6,8 +8,50 @@ export class BowlingGame {
     }
 
     calculateTotalScore(): number {
-        return this.rolls.reduce((score, pins) => {
-            return score + pins;
-        }, 0);
+        let totalScore = 0;
+        let rollIndex = 0;
+
+        for (let frameIndex = 0; frameIndex < this.maxFrames; frameIndex++) {
+            const frameScore = this.calculateFrameScore({ totalScore, rollIndex });
+            totalScore = frameScore.totalScore;
+            rollIndex = frameScore.rollIndex;
+        }
+
+        return totalScore;
+    }
+
+    private calculateFrameScore({ totalScore, rollIndex }: { totalScore: number, rollIndex: number }) {
+
+        if (this.isStrike(rollIndex)) {
+            return {
+                totalScore: totalScore + this.maxScorePerFrame + this.strikeBonus(rollIndex),
+                rollIndex: rollIndex + 1
+            };
+        }
+        if (this.isSpare(rollIndex)) {
+            return {
+                totalScore: totalScore + this.maxScorePerFrame + this.spareBonus(rollIndex),
+                rollIndex: rollIndex + 2
+            };
+        }
+        return {
+            totalScore: totalScore + this.sumBallsInFrame(rollIndex),
+            rollIndex: rollIndex + 2
+        }
+    }
+    private isStrike(frameIndex: number): boolean {
+        return this.rolls[frameIndex] === this.maxScorePerFrame;
+    }
+    private isSpare(frameIndex: number): boolean {
+        return this.sumBallsInFrame(frameIndex) === this.maxScorePerFrame;
+    }
+    private strikeBonus(frameIndex: number): number {
+        return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+    }
+    private spareBonus(frameIndex: number): number {
+        return this.rolls[frameIndex + 2];
+    }
+    private sumBallsInFrame(frameIndex: number): number {
+        return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
     }
 }
